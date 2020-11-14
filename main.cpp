7c95b5223416b666b0fb4531081ca5689bf1eb5f
mrblug <40170347+mrblug@users.noreply.github.com>
2020-11-14 15:57:42 +0800

    Shushi Problem
    Algorithm Competition Page41

--- main.cpp
+++ main.cpp
@@ -1,15 +1,57 @@
 #include <iostream>
+#include <algorithm>
+
+int num[10];
+
+inline bool pass(int x){
+    for(; x > 0; x /= 10)
+        if(num[x % 10] - 1)
+            return false;
+    return true;
+}
+
+inline int len_of_num(int x){
+    int cnt = 0;
+    if(x)
+        for(; x > 0; ++cnt, x /= 10);
+    else
+        cnt = 1;
+    return cnt;
+}
+
+inline void print(int num, char ch){
+    for(; num >= 1; putchar(ch), --num);
+}
 
 int main(){
-    int a, b, c, i, kase = 0;
-    for(; scanf("%d%d%d", &a, &b, &c) == 3; ){
-        printf("Case %d: ", ++kase);
+    std::ios::sync_with_stdio(false);
 
-        for(i = 10; i <= 100; ++i){
-            if( !((i - a) % 3) && !((i - b) % 5) && !((i - c) % 7) )
-                break;
+    std::string s;
+    std::cin >> s;
+    int cnt = 0;
+    for(auto &a : s)
+        if(a - '0' >= 0 && a - '0' <= 9) num[a - '0'] = 1;
+    for(int x = 100; x <= 999; ++x){
+        if(!pass(x)) continue;
+        for(int y = 10; y <= 99; ++y){
+            if(!pass(y)) continue;
+            if(!pass(x * (y % 10) )) continue;
+            if(!pass(x * (y / 10) )) continue;
+            if(!pass(x * y) ) continue;
+
+            int len = std::max(len_of_num(x * (y / 10)) + 1, len_of_num(x * y));
+            print(8, '*'); putchar('\n');
+            print(len - 3, ' '); printf("%d\n", x);
+            print(1, 'X'); print(len - 3, ' '); printf("%d\n", y);
+            print(len, '-'); printf("\n");
+            print(len - len_of_num(x * (y % 10)), ' '); printf("%d\n", x * (y % 10));
+            print(len - len_of_num(x * (y / 10)) - 1, ' '); printf("%d\n", x * (y / 10));
+            print(len, '-'); printf("\n");
+            print(len - len_of_num(x * y), ' '); printf("%d\n********\n", x * y);
+
+            ++cnt;
         }
-        i > 100 ? printf("No answer\n") :  printf("%d\n", i); ;
     }
+    printf("The number of solutions:%d\n", cnt);
     return 0;
 }
